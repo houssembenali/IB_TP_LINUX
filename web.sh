5b1835b63021e2ef322b88c8bd3fc029bcb22ddc
Houssem Eddine AMOR BEN ALI <houssembenali90@gmail.com>
2021-04-26 15:06:09 +0200

    Créer page index.html #3

--- web.sh
+++ web.sh
@@ -13,6 +13,10 @@ YELLOW='\033[0;33m'	# Yellow Color
 GREEN='\033[0;32m'	# Grean Color
 NC='\033[0m' 		# No Color
 #BOLD=$(tput bold)	# Ecrire en Gras
+HTML_INDEX_CONTENT='!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"><html><head><title>Ma première page avec du style</title></head><body><!-- Menu de navigation du site -->
+<ul class="navbar"><li><a href="index.html">Home page</a></ul><!-- Contenu principal --><h1>Ma première page avec du style</h1><p>Bienvenue sur ma page avec du style!
+<p>Il lui manque des images, mais au moins, elle a du style. Et elle a desliens, même s''ils ne mènent nulle part...&hellip;<p>Je devrais étayer, mais je ne sais comment encore.
+<!-- Signer et dater la page, c''est une question de politesse! --><address>Fait le 22 avril 2021<br>par moi.</address></body></html>'
 
 ###### Fonctions
 
@@ -52,4 +56,6 @@ w_install_package "apache2"
 # droit r/w du dossier WWW
 echo "${GREEN}$(date +'%Y-%m-%d %H:%M:%S') [ INFO  ] : Réglage des droit du répertoir WWW d'apach2"
 chown -R www-data:www-data "/var/www/html"
-
+#génération de la page index.html
+echo "${GREEN}$(date +'%Y-%m-%d %H:%M:%S') [ INFO  ] : Génération de page index /var/www/html/index.html"
+echo $HTML_INDEX_CONTENT > /var/www/html/index.html
