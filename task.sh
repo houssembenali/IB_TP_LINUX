91949d14470bf7df8cfcf78c87b811163d548230
Zabasux <arnaud.degez@neo-soft.fr>
2021-04-30 01:00:22 +0200

    CRON sauvegarde /7 jours (suppression des fichier > 7 jours)

--- task.sh
+++ task.sh
@@ -11,6 +11,8 @@ ws_creation_file_allow() {
     if ! test -f /etc/cron.allow ; then
     touch /etc/cron.allow
     echo "00 */1 * * * /home/vagrant/save.sh > /home/vagrant/save.log"
+    echo "* * /7 * * find . -ctime +7 -exec rm -f /home/save/server_ic/server_ic-*;"
+    echo "* * /7 * * find . -ctime +7 -exec rm -f /home/save/web/server_web-*;"
     fi
 }
 
